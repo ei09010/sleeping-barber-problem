c43a38b74d285d0cf22003eec1bfbdddbcbb5ccc
mario <mario24jsp@gmail.com>
2021-05-06 00:33:41 +0100

    sleeping barber base solution proposal

--- main.go
+++ main.go
@@ -9,12 +9,16 @@ import (
 const (
 	sleeping = iota
 	cutting
+	checking
 )
 
+var wg *sync.WaitGroup
+
 type Barber struct {
-	name string
+	name                  string
+	customerBeingAttended *Customer
 	sync.Mutex
-	state int // sleeping or cutting
+	state int // sleeping, cutting or checking
 }
 
 type Customer struct {
@@ -23,27 +27,30 @@ type Customer struct {
 
 func NewBarber() *Barber {
 	return &Barber{
-		name:  "The Barber",
+		name:  "Bob the Barber",
 		state: sleeping,
 	}
 }
 
-func BarberWork(waitingCustomers chan *Customer, b *Barber) {
+func barberWork(waitingCustomers chan *Customer, b *Barber) {
 
 	for {
 		b.Lock()
 		defer b.Unlock()
-		b.state = sleeping
+		b.state = checking
+		b.customerBeingAttended = nil
 
-		fmt.Printf("Currently, there are %v clients in the waiting room", len(waitingCustomers))
+		fmt.Printf("Currently, there are %v clients in the waiting room \n", len(waitingCustomers))
+		time.Sleep(time.Millisecond * 100)
 
 		select {
 		case c := <-waitingCustomers:
-			DoesHairCut(c, b)
-
+			doesHairCut(c, b)
+			b.Unlock()
 		default:
-			fmt.Println("The barber is sleeping")
+			fmt.Println("The barber is sleeping because the waiting room is empty")
 			b.state = sleeping
+			b.customerBeingAttended = nil
 			b.Unlock()
 
 		}
@@ -52,18 +59,86 @@ func BarberWork(waitingCustomers chan *Customer, b *Barber) {
 
 }
 
-func DoesHairCut(c *Customer, b *Barber) {
+func doesHairCut(c *Customer, b *Barber) {
 	b.state = cutting
+
+	time.Sleep(time.Millisecond * 100)
+
+	b.customerBeingAttended = c
 
 	b.Unlock()
 
-	fmt.Printf("Cutting hair to %v", c.name)
+	fmt.Printf("Cutting hair to %v \n", c.name)
 	time.Sleep(time.Millisecond * 100)
-	fmt.Printf("Hair cut to %v is finished", c.name)
+	fmt.Printf("Hair cut to %v is finished \n", c.name)
 
 	b.Lock()
+	b.customerBeingAttended = nil
+	wg.Done()
+}
+
+func customerArrivesToBarberShop(waitingCustomers chan<- *Customer, c *Customer, b *Barber) {
+
+	b.Lock()
+
+	defer b.Unlock()
+
+	switch b.state {
+
+	case sleeping:
+		select {
+		case waitingCustomers <- c:
+		default:
+			wg.Done()
+		}
+
+	case cutting:
+		select {
+		case waitingCustomers <- c:
+		default:
+			fmt.Println("Waiting room is full, customer called ", c.name, " is leaving")
+			wg.Done()
+		}
+
+	case checking:
+		fmt.Println("Customer called ", c.name, " bumped into the barber while he was checking the waiting room, he will wait")
+		waitingCustomers <- c
+	}
+
 }
 
 func main() {
 
+	barber := NewBarber()
+
+	waitingRoomChairs := 5
+
+	waitingRoom := make(chan *Customer, waitingRoomChairs)
+
+	go barberWork(waitingRoom, barber)
+
+	wg = new(sync.WaitGroup)
+
+	for _, c := range customerGenerator(5) {
+
+		wg.Add(1)
+
+		go customerArrivesToBarberShop(waitingRoom, c, barber)
+	}
+
+	wg.Wait()
+
+	fmt.Println("All customers were sorted")
+
+}
+
+func customerGenerator(customerAmount int) []*Customer {
+
+	customerSliceToReturn := make([]*Customer, customerAmount)
+
+	for i := 0; i < customerAmount; i++ {
+		customerSliceToReturn[i] = &Customer{name: fmt.Sprintf("Customer-%v", i)}
+	}
+
+	return customerSliceToReturn
 }
